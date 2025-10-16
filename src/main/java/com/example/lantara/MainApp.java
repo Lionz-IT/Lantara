882575ad912c76094de3e7a06e4c8f4a2b564ad2
Rafif Ahmad Yudhistira <raymlg2005@gmail.com>
2025-10-16 15:17:14 +0700

    penambahan ikon aplikasi

--- src/main/java/com/example/lantara/MainApp.java
+++ src/main/java/com/example/lantara/MainApp.java
@@ -6,21 +6,27 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import com.example.lantara.model.DatabaseHelper;
 
+// 1. Import kelas Image
+import javafx.scene.image.Image;
+
 import java.io.IOException;
 
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("view/landing-page-view.fxml"));
-        // Atur ukuran scene menjadi 1280x720
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720); 
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("LANTARA");
+
+        // 2. Tambahkan baris ini untuk mengatur ikon
+        stage.getIcons().add(new Image(MainApp.class.getResourceAsStream("view/assets/logo.png")));
+
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        DatabaseHelper.initializeDatabase(); 
+        DatabaseHelper.initializeDatabase();
         launch(args);
     }
 }
